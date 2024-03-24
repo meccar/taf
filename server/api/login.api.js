39c151652891ff2752a5394da8e8c12ecb0459e7
Tung Doan <tung14hn@gmail.com>
2024-03-24 08:34:18 +0000

    updated token

--- server/api/login.api.js
+++ server/api/login.api.js
@@ -1,5 +1,6 @@
 const Account = require("../models/account.models.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.loginForm = async (req, res, next) => {
   try {
@@ -12,13 +13,15 @@ exports.loginForm = async (req, res, next) => {
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
-    if (passwordMatch) {
-      res.status(200).json({ message: "Login successful" });
-    } else {
+    if (!passwordMatch) {
       res.status(401).json({ message: "Invalid credentials" });
     }
+    const token = jwt.sign({ userId: user.id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ message: "Login successful: " + token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Login failed: " + error.message });
   }
 };
