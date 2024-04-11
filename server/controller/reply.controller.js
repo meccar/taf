af5bc1b0abee80dcbf7cb63e82259e9cd8fc92bc
meccar <tung14hn@gmail.com>
2024-04-11 12:27:45 +0000

    updated error handler

--- server/controller/reply.controller.js
+++ server/controller/reply.controller.js
@@ -1,32 +1,26 @@
 /* eslint-disable prettier/prettier */
 const JWT = require("../token/jwt");
 const Reply = require("../models/reply.models");
+const catchAsync = require("../util/catchAsync");
 
-class ReplyController {
-  async reply(req, res) {
-    try {
-      const { text, communityId } = req.body;
-      const [decoded] = await Promise.all([
-        JWT.decodedToken(req.cookies.token),
-      ]);
-      // Create a new reply instance
-      const newReply = await Reply.create({
-        comment_id: communityId,
-        user_id: decoded.user_id,
-        text: text,
-      });
+exports.reply = catchAsync(async (req, res, next) => {
+  const { text, communityId } = req.body;
+  const [decoded] = await Promise.all([JWT.decodedToken(req.cookies.token)]);
+  // Create a new reply instance
+  const newReply = await Reply.create({
+    comment_id: communityId,
+    user_id: decoded.user_id,
+    text: text,
+  });
 
-      res.status(201).json({ status: "success", message: "Reply successful", data: { newReply } });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ status: "fail", message: error.message });
-    }
-  }
-  
-  async getReplyByComment(id) { 
-    const [replies] = await Promise.all([Reply.findOne({ comment_id: id })]);
-    return replies;
-  }
-}
+  res.status(201).json({
+    status: "success",
+    message: "Reply successful",
+    data: { newReply },
+  });
+});
 
-module.exports = new ReplyController();
+exports.getReplyByComment = async (id) => {
+  const [replies] = await Promise.all([Reply.findOne({ comment_id: id })]);
+  return replies;
+};
