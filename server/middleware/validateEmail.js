4eff50d5e44992cb1812483158b86eda0e2e4e5b
meccar <tung14hn@gmail.com>
2024-04-12 08:21:32 +0000

    updated error handling

--- server/middleware/validateEmail.js
+++ server/middleware/validateEmail.js
@@ -1,44 +1,39 @@
 const Account = require("../models/account.models");
 const VerifyMail = require("../models/verify_mail.models");
+const catchAsync = require("../util/catchAsync");
+const AppError = require("../util/appError");
 
-const validateEmail = async (req, res, next) => {
-  // Check if account exists and email is verified concurrently
-  try {
-    const verification = await VerifyMail.findOne({
-      email: req.params.email,
-      secret_code: req.params.secretCode,
-    });
+exports.validateEmail = catchAsync(async (req, res, next) => {
+  const verification = await VerifyMail.findOne({
+    email: req.params.email,
+    secret_code: req.params.secretCode,
+  });
 
-    if (Date.now() > verification.expires_at) {
-      this.sendMail(req, res, verification.email);
-      return res.status(400).json({
-        error:
-          "Verification code expired. Please check your email for the latest one.",
-      });
-    }
-
-    if (!verification) {
-      return res.status(404).json({ message: "Invalid verification details" });
-    }
-
-    // Check if verification has already been used
-    if (verification.is_used) {
-      return res.status(400).json({ message: "Account already registered" });
-    }
-
-    verification.is_used = true;
-    await verification.save();
-
-    // Find the associated account using the email from verification
-    const account = await Account.findOne({ email: verification.email });
-
-    // Update the account's email verification status
-    account.is_email_verified = true;
-    await account.save();
-    next();
-  } catch (error) {
-    next(error); // Pass the error to the error handling middleware
+  if (Date.now() > verification.expires_at) {
+    this.sendMail(req, res, verification.email);
+    return new AppError(
+      "Verification code expired. Please check your email for the latest one.",
+      400,
+    );
   }
-};
 
-module.exports = validateEmail;
+  if (!verification) {
+    return new AppError("Invalid verification details", 404);
+  }
+
+  // Check if verification has already been used
+  if (verification.is_used) {
+    return new AppError("Account already registered", 400);
+  }
+
+  verification.is_used = true;
+  await verification.save();
+
+  // Find the associated account using the email from verification
+  const account = await Account.findOne({ email: verification.email });
+
+  // Update the account's email verification status
+  account.is_email_verified = true;
+  await account.save();
+  next();
+});
