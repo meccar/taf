75c6caaaa39fb791945f9a98332c963d772281cb
meccar <tung14hn@gmail.com>
2024-04-26 10:59:36 +0800

    added kafka

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const path = require("path");
+const catchAsync = require("../util/catchAsync")
+import {kafka} from "kafkajs";
 
 require("dotenv").config({ path: "./app.env" });
 
-const Config = {
+exports = Config = {
   privateKey: fs.readFileSync(
     path.join(__dirname, "..", "id_rsa_priv.pem"),
     "utf8",
@@ -23,4 +25,39 @@ const Config = {
   },
 };
 
-module.exports = Config;
+exports = class kafkaConfig {
+  constructor(req){
+    this.kafka = new kafka({
+      clientId: "nodejs-kafka",
+      brokers: [req.get("host")]
+    })
+    this.producer = this.kafka.producer();
+    this.consumer = this.kafka.consumer({groupId: "test-group"})
+
+    this.produce = catchAsync( async (topic, messages) => {
+        await this.producer.connect();
+        await this.producer.send({
+          topic: topic,
+          messages: messages,
+        });
+
+        await this.producer.disconnect();
+    })
+
+    this.consume = catchAsync( async (topic, callback) => {
+      await this.consumer.connect();
+      await this.consumer.subscribe({
+        topic: topic,
+        fromBeginning: true,
+      });
+      await this.consumer.run({
+        matchMessage: async ({topic, partition, message}) => {
+          const value = message.value.toString();
+          callback(value);
+        }
+      });
+      await this.consumer.disconnect();
+    });
+  }
+}
+// module.exports = Config;
