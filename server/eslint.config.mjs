6cc57ccd786669540748e9375ec378cd1b4f455a
Tung <tung14hn@gmail.com>
2024-04-26 18:02:33 +0800

    updated eslint

---
+++ server/eslint.config.mjs
@@ -0,0 +1,42 @@
+import globals from "globals";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends("standard"),
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+      "spaced-comment": "off",
+      "no-console": "off",
+      "consistent-return": "off",
+      "func-names": "off",
+      "object-shorthand": "off",
+      "no-process-exit": "off",
+      "no-param-reassign": "off",
+      "no-return-await": "off",
+      "no-underscore-dangle": "off",
+      "class-methods-use-this": "off",
+      "prefer-destructuring": ["error", { object: true, array: false }],
+      "no-unused-vars": ["error", { argsIgnorePattern: "req|res|next|val" }],
+    },
+  },
+];
